cd22d1b05e7534e85525667c1ffb3d68bdaf37a9
stewmckendry <stewart.mckendry@gmail.com>
2025-11-30 21:57:46 +0000

    Restructure as proper Python package

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,4 @@
-import sys
-sys.path.insert(0, 'ci-cd-learning/src')
-
 def test_add():
-    from calculator import add
+    from calculator_pkg.calculator import add
     assert add(2,2) == 4
     assert add(2,1) != 4
